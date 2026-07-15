722dd0cb3c32589ef4f440b0da291cc8928681fe
solmasta <lukedorsett@hotmail.com>
2026-07-15 17:48:24 -0500

    Add Auth class with registration and login methods

---
+++ functions/auth.js
@@ -0,0 +1,21 @@
+export class Auth {
+  constructor(env) {
+    this.env = env;
+  }
+  
+  async register(email, password) {
+    // Implementation
+  }
+  
+  async login(email, password) {
+    // Implementation
+  }
+  
+  async verifyToken(token) {
+    // Implementation
+  }
+  
+  async verifyApiKey(apiKey) {
+    // Implementation
+  }
+}
